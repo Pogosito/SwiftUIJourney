b3c1e5748ffd921104df93359a0b1c8436cdedd1
Pogosito <pogos.anesyan0@gmail.com>
2023-12-01 13:50:23 +0300

    Remeber what i can use Label for image + text and learn how to create ButtonStyle

---
+++ LongSwiftUJourney/Chapter 6/GradientButtonStyle.swift
@@ -0,0 +1,29 @@
+//
+//  GradientButtonStyle.swift
+//  LongSwiftUJourney
+//
+//  Created by Анесян Погос Артурович on 01.12.2023.
+//
+
+import SwiftUI
+
+struct GradientButtonStyle: ButtonStyle {
+
+	func makeBody(configuration: Configuration) -> some View {
+		configuration.label
+			.frame(minWidth: 0, maxWidth: .infinity)
+			.padding()
+			.foregroundColor(Color.white)
+			.background(
+				LinearGradient(
+					colors: [Color("DarkGreen"), Color("LightGreen")],
+					startPoint: .leading,
+					endPoint: .trailing
+				)
+			)
+			.cornerRadius(40)
+			.padding(.horizontal, 20)
+			.scaleEffect(configuration.isPressed ? 0.5 : 1)
+	}
+}
+
